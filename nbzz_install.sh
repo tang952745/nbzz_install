7e95c4c420ee6bf3db366e6eaf2e9a1ec3371674
tomin_wsl <touxiaoling@outlook.com>
2021-08-15 17:12:29 +0800

    fix nbzz patgh

--- nbzz_install.sh
+++ nbzz_install.sh
@@ -4,6 +4,7 @@ apt upgrade -y
 # Install Git
 apt install git -y
 
+rm -rf ./nbzz
 # Checkout the source and install
 git clone https://github.com/LeetSquad/Swarm-nbzz.git nbzz
 cd nbzz
@@ -15,3 +16,5 @@ sh install.sh
 cd ..
 
 curl  -fsSL https://gitee.com/tousang/nbzz_install/raw/master/nbzz_run.py | python3 -
+
+#curl  -fsSL https://gitee.com/tousang/nbzz_install/raw/master/nbzz_install.sh | bash -
