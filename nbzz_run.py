25ac1a51de5d3a5e1ae8327c920eaee3ac467a3f
tomin_wsl <touxiaoling@outlook.com>
2021-08-15 16:09:48 +0800

    fix

--- nbzz_run.py
+++ nbzz_run.py
@@ -23,11 +23,16 @@ if not bee_con_path.exists():
     exit(1)
 with bee_con_path.open("r",) as fid :
     bee_con=yaml.safe_load(fid)
+
 bee_install_path=Path(bee_con["bee"]["base_path"])
 bee_passwd=bee_con["bee"]["password"]
+
 if not bee_install_path.exists():
     print("bee未安装或者未成功启动")
     exit(1)
 
 for i_bee_path in tqdm(bee_install_path.glob(".bee*")):
-    print(i_bee_path)
+    print(f"install bee in {i_bee_path}")
+    faucet(bee_passwd,i_bee_path)
+    pledge(bee_passwd,i_bee_path)
+    
