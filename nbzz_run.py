3a9955302479af5ff7b1f6483c137c88904e0f8f
tomin_wsl <touxiaoling@outlook.com>
2021-08-16 12:01:41 +0800

    add check

--- nbzz_run.py
+++ nbzz_run.py
@@ -1,7 +1,11 @@
 try:
     from nbzz.cmds.pledge_funcs import faucet, pledge
-    from nbzz.util.bee_key import decrypt_privatekey_from_bee_keyfile
+    from nbzz.util.config import load_config
     import eth_keyfile
+    from web3 import Web3
+    from typing import Dict
+    from nbzz.util.default_root import DEFAULT_ROOT_PATH
+    from nbzz.util.nbzz_abi import NBZZ_ABI
 except:
     print("nbzz未安装,此脚本需要安装nbzz 然后 . ./activate")
 import yaml
@@ -33,16 +37,37 @@ bee_passwd=bee_con["bee"]["password"]
 if not bee_install_path.exists():
     print("bee未安装或者未成功启动")
     exit(1)
-all_bee_path=[i for i in bee_install_path.glob(".bee*")]
 
+
+class w3:
+    def __init__(self,address):
+        #print(tx_receipt.blockNumber)
+        self.w3= Web3(Web3.WebsocketProvider("ws://120.76.247.190:8546"))
+        config: Dict = load_config(DEFAULT_ROOT_PATH, "config.yaml")
+        self.nbzz_contract = self.w3.eth.contract(address=config["network_overrides"]["constants"][config["selected_network"]]["CONTRACT"],abi=NBZZ_ABI)
+        
+
+        self.address=Web3.toChecksumAddress("0x"+address)
+    def balanceOf(self):
+        balance=self.nbzz_contract.functions.balanceOf(self.address).call()
+        return balance
+    def pledge_banlance(self):
+        balance=self.nbzz_contract.functions.addressPledge(self.address).call()
+        return balance
+
+all_bee_path=[i for i in bee_install_path.glob(".bee*")]
 for i_bee_path in tqdm(all_bee_path):
     swarm_key=i_bee_path/"keys"/"swarm.key"
     if swarm_key.exists():
+        geth_address=eth_keyfile.load_keyfile(str(swarm_key))["address"]
+        eth_stat=w3(geth_address)
+        if eth_stat.pledge_banlance() >=15:
+            print(f"{i_bee_path} 已经完成质押并启动")
+            continue
         print(f"install bee in {i_bee_path}")
 
-        geth_address=eth_keyfile.load_keyfile(str(swarm_key))
-        print(geth_address)
-        exit(1)
+        
+
         try:
             faucet(bee_passwd,str(swarm_key))
         except: 
